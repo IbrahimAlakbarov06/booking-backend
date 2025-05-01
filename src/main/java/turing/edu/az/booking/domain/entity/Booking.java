233c6d0af5cb12f734846157aec25d4e14663453
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2025-05-01 23:38:02 +0400

    Booking model created

---
+++ src/main/java/turing/edu/az/booking/domain/entity/Booking.java
@@ -0,0 +1,28 @@
+package turing.edu.az.booking.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id", nullable = false)
+    private Flight flightId;
+
+    @Column(name = "passenger_name", nullable = false)
+    private String passengerName;
+
+    @Column(name = "number_of_seats", nullable = false)
+    private Integer numberOfSeats;
+}
