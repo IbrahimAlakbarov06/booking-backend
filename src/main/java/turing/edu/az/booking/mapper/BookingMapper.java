1a5ae107650744c023f444ecbff52fd877d7f3d3
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2025-05-03 14:20:27 +0400

    Mapper interfaces created

--- src/main/java/turing/edu/az/booking/mapper/BookingMapper.java
+++ src/main/java/turing/edu/az/booking/mapper/BookingMapper.java
@@ -1,4 +1,28 @@
 package turing.edu.az.booking.mapper;
 
-public class BookingMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import turing.edu.az.booking.domain.entity.Booking;
+import turing.edu.az.booking.domain.entity.Flight;
+import turing.edu.az.booking.model.response.BookingDto;
+import turing.edu.az.booking.model.response.FlightDto;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper extends EntityMapper<BookingDto, Booking> {
+
+    BookingMapper INSTANCE = Mappers.getMapper(BookingMapper.class);
+
+    @Override
+    BookingDto toEntity(Booking dto);
+
+    @Override
+    List<BookingDto> toEntity(List<Booking> dtoList);
+
+    @Override
+    Booking toDto(BookingDto entity);
+
+    @Override
+    List<Booking> toDto(List<BookingDto> entityList);
 }
--- src/main/java/turing/edu/az/booking/mapper/FlightMapper.java
+++ src/main/java/turing/edu/az/booking/mapper/FlightMapper.java
@@ -1,4 +1,26 @@
 package turing.edu.az.booking.mapper;
 
-public class FlightMapper {
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import turing.edu.az.booking.domain.entity.Flight;
+import turing.edu.az.booking.model.response.FlightDto;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface FlightMapper extends EntityMapper<FlightDto, Flight> {
+
+    FlightMapper INSTANCE = Mappers.getMapper(FlightMapper.class);
+
+    @Override
+    FlightDto toEntity(Flight dto);
+
+    @Override
+    List<FlightDto> toEntity(List<Flight> dtoList);
+
+    @Override
+    Flight toDto(FlightDto entity);
+
+    @Override
+    List<Flight> toDto(List<FlightDto> entityList);
 }
