f5efe41173314ade4cd954b61f6aa161831ff553
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2025-05-03 14:51:54 +0400

    Passenger added

--- src/main/java/turing/edu/az/booking/domain/entity/Passenger.java
+++ src/main/java/turing/edu/az/booking/domain/entity/Passenger.java
@@ -1,4 +1,25 @@
 package turing.edu.az.booking.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "passengers")
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fullname", nullable = false)
+    private String fullName;
+
+    @Column(name = "email")
+    private String email;
+
 }
